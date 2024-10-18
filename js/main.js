a4e86f980e0d3d09f19fe4b4b7f10396bd040d49
Hesam Samimi <hesam.samimi.cs@gmail.com>
2024-10-18 01:30:00 +0000

    up

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-async function blah(): Promise<void> {
+'use strict';
+async function blah() {
   // const targetUrl = encodeURIComponent('http://api.no-cors.com/data?foo=bar');
   const targetUrl =
     'https://www.googleapis.com/books/v1/volumes?q=time&printType=magazines';
   const targetUrlC = encodeURIComponent(targetUrl);
   // const proxyUrlC = 'https://cors.learningfuze.com/?url=' + targetUrlC;
-
   // Direct fetch:
   /*
-  try {
-    const response = await fetch(targetUrl);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    try {
+      const response = await fetch(targetUrl);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
     }
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-  */
-
+    */
   // HTTP Request Fetch - LFZ Proxy Coors Server:
   try {
     const response = await fetch(
@@ -32,18 +31,16 @@ async function blah(): Promise<void> {
   } catch (error) {
     console.log(error);
   }
-
   // HTTP Request Ajax - LFZ Proxy Coors Server:
   /*
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', proxyUrlC);
-  xhr.setRequestHeader('token', 'abc123');
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    console.log(xhr.response);
-  });
-  xhr.send();
-  */
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', proxyUrlC);
+    xhr.setRequestHeader('token', 'abc123');
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      console.log(xhr.response);
+    });
+    xhr.send();
+    */
 }
-
 blah();
