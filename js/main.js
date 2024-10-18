b5b2cc94c9a6db1c5cbd94aa94dd711e0893ee34
Hesam Samimi <hesam.samimi.cs@gmail.com>
2024-10-18 02:36:20 +0000

    fetch cors

--- js/main.js
+++ js/main.js
@@ -1,27 +1,13 @@
 'use strict';
-async function blah() {
+// HTTP Request - Fetch - LFZ Proxy Cors Server:
+async function fetchDataWithFetch() {
   // const targetUrl = encodeURIComponent('http://api.no-cors.com/data?foo=bar');
-  const targetUrl =
-    'https://www.googleapis.com/books/v1/volumes?q=time&printType=magazines';
-  const targetUrlC = encodeURIComponent(targetUrl);
-  // const proxyUrlC = 'https://cors.learningfuze.com/?url=' + targetUrlC;
-  // Direct fetch:
-  /*
-    try {
-      const response = await fetch(targetUrl);
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-    */
-  // HTTP Request Fetch - LFZ Proxy Coors Server:
+  const targetUrl = encodeURIComponent(
+    'https://www.googleapis.com/books/v1/volumes?q=time&printType=magazines',
+  );
   try {
     const response = await fetch(
-      'https://cors.learningfuze.com/?url=' + targetUrlC,
+      'https://cors.learningfuze.com/?url=' + targetUrl,
     );
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -31,16 +17,22 @@ async function blah() {
   } catch (error) {
     console.log(error);
   }
-  // HTTP Request Ajax - LFZ Proxy Coors Server:
-  /*
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', proxyUrlC);
-    xhr.setRequestHeader('token', 'abc123');
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      console.log(xhr.response);
-    });
-    xhr.send();
-    */
 }
-blah();
+// HTTP Request - Ajax - LFZ Proxy Cors Server:
+function fetchDataWithAjax() {
+  // const targetUrl = encodeURIComponent('http://api.no-cors.com/data?foo=bar');
+  const targetUrl = encodeURIComponent(
+    'https://www.googleapis.com/books/v1/volumes?q=time&printType=magazines',
+  );
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://cors.learningfuze.com/?url=' + targetUrl);
+  xhr.setRequestHeader('token', 'abc123');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    const data = xhr.response;
+    console.log(data);
+  });
+  xhr.send();
+}
+fetchDataWithFetch();
+fetchDataWithAjax();
