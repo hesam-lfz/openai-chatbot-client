c5ddf2c301496ba5b7175092dbc01fa95b59cd81
Hesam Samimi <hesam.samimi.cs@gmail.com>
2024-11-15 05:17:29 +0000

    up

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# front-end-project
+# openai-chatbot
 
-A dynamic HTML, CSS, and TypeScript solo project.
+An OpenAI chatbot client
--- js/main.js
+++ js/main.js
@@ -4,9 +4,15 @@ const client = new OpenAI({
 });
 async function main() {
   const chatCompletion = await client.chat.completions.create({
-    messages: [{ role: 'user', content: 'Say this is a test' }],
+    messages: [
+      {
+        role: 'user',
+        content:
+          'In the context of Hamlet, what does to be or not to be refer to?',
+      },
+    ],
     model: 'gpt-3.5-turbo',
   });
-  console.log(chatCompletion);
+  console.log(chatCompletion.choices[0].message);
 }
 main();
