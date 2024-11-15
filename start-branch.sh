64478cbabc797e57d18b01738e32abd81cc49a9c
Hesam Samimi <hesam.samimi.cs@gmail.com>
2024-11-15 05:22:27 +0000

    up

--- start-branch.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-git switch main
-git pull origin main
-git branch $1
-git branch --list
-git switch $1
-
--- submit-branch.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-git switch $1
-git status
-git commit -a -m "Completed $1 exercise."
-git push origin $1
-git switch main
